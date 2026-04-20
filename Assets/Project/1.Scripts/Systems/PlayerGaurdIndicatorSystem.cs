5b335e6a4b3a3038890058dc1a160baf0ebf7a50
rlawkdgn123 <63436456+rlawkdgn123@users.noreply.github.com>
2026-04-20 14:20:55 +0900

    기능: 숄더 카메라와 가드 인디케이터 배선

---
+++ Assets/Project/1.Scripts/Systems/PlayerGaurdIndicatorSystem.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGaurdIndicatorSystem : MonoBehaviour
+{
+    [SerializeField] private PlayerGuardIndicator[] m_playerGuardIndicators;
+    [SerializeField] private Image[] m_playerGuardIndicatorImages;
+    [SerializeField] private PlayerController m_playerController;
+    [SerializeField] private CursorManager m_cursorManager;
+
+    [Range(0f, 1f), Tooltip("기본 달리기 속도.")]
+    [SerializeField] private float m_OffAlpha = 0.3f;
+
+    private void Awake()
+    {
+        m_playerGuardIndicators = GetComponentsInChildren<PlayerGuardIndicator>();
+        m_playerGuardIndicatorImages = GetComponentsInChildren<Image>();
+
+        // 플레이어 컨트롤러
+        if (!m_playerController)
+            m_playerController =  GetComponentInParent<PlayerController>();
+        if (!m_playerController) Debug.LogError("[PlayerGaurdIndicatorSystem] PlayerController 할당이 되지 않았습니다.");
+
+
+        // 커서매니저
+        if (!m_cursorManager)
+            m_cursorManager = GameObject.Find("CursorManager").GetComponent<CursorManager>();
+
+        if (!m_cursorManager) Debug.LogError("[CursorManager] CursorManager 할당이 되지 않았습니다.");
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        GuardIndicatorStateUpdate();
+    }
+
+    // 투명도 조절
+    private void SetOpacity(Image image, float opacity)
+    {
+        Color color = image.color;
+        color.a = opacity;
+        image.color = color;
+    }
+
+    void GuardIndicatorStateUpdate()
+    {
+
+        for (int i = 0; i < m_playerGuardIndicators.Length; ++i)
+        {
+            PlayerGuardIndicator indicator = m_playerGuardIndicators[i];
+            Image image = m_playerGuardIndicatorImages[i];
+
+            switch (indicator.GetHealthState())
+            {
+                case GuardIndicator.IndicatorHealthState.Weak:  image.color = Color.orangeRed; break;
+                case GuardIndicator.IndicatorHealthState.Break: image.color = Color.black; break;
+                default: image.color = Color.green; break;
+            }
+
+            switch (indicator.GetActionState())
+            {
+                case GuardIndicator.IndicatorActionState.Attack: image.color = Color.darkRed; break;
+                case GuardIndicator.IndicatorActionState.Parry:  image.color = Color.orangeRed; break;
+                default: break;
+            }
+
+            // 주의 : 커서매니저의 GuardZone() : 현재 마우스에 따른 위치
+            // 주의 : 인디케이터의 GuardZone() : 사전 설정된 값 (스스로 표기용)
+            if (CursorManager.Instance.GetGuardZone() == indicator.GetGuardZone()) // 현재 값이 같다면
+                SetOpacity(image, 1f);
+            else
+                SetOpacity(image, m_OffAlpha);
+        }
+    }
+}
--- Assets/Project/1.Scripts/UI/PlayerHUD/PlayerGuardIndicator.cs
+++ Assets/Project/1.Scripts/UI/PlayerHUD/PlayerGuardIndicator.cs
@@ -1,101 +1,214 @@
 ﻿/*
  * 플레이어 가드 인디케이터
+ * - 이미지 UI 본체에 붙임
  * - UI 플레이어 추적
  */
 
+using GuardIndicator;
+using NUnit.Framework;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
+
+namespace GuardIndicator{
+
+    public enum IndicatorHealthState : int
+    {
+        Default = 0,
+        Weak,
+        Break,
+    }
+
+    public enum IndicatorActionState : int
+    {
+        Default = 0,
+        Attack,
+        Parry,
+    }
+
+    [Serializable]
+    public class Components
+    {
+        [Header("GuardIndicator")]
+        public RectTransform RectTransform;
+        public Animator Animator;
+
+        [Header("IndicatorAnchor")]
+        public Transform IndicatorAnchor;
+
+        [Space]
+        [Header("IndicatorCanvas")]
+        public Canvas IndicatorCanvas;
+
+        [Space]
+        [Header("OtherComponents")]
+        public PlayerController PlayerController;
+        public Camera MainCamera;
+    }
+
+    [Serializable]
+    public class CheckOption
+    {
+        public CursorManager.GuardZone GuardZone;
+
+        [Header("Offset")]
+        public Vector3 IndicatorOffset;
+
+        [Header("Direction")]
+        [Tooltip("시작 및 기본 가드 방향")]
+        public const int DefaultGuardDirection = (int)CursorManager.GuardZone.Right;
+    }
+
+    [Serializable]
+    public class CurrentValue
+    {
+        [Header("Stats")]
+        [Tooltip("가드 체력.")]
+        public int ArmorHealth;
+
+    }
+
+    [Serializable]
+    public class CurrentState
+    {
+
+        [Header("Direction")]
+        [Tooltip("커서가 해당하는 존에 입성했는지")]
+        public bool IsActive;
+
+        [Space]
+        [Header("Direction")]
+        [Tooltip("체력에 따른 게이지 상태")]
+        public GuardIndicator.IndicatorHealthState HealthState; // 체력에 따른 게이지 상태
+        [Tooltip("행동에 따른 게이지 상태")]
+        public GuardIndicator.IndicatorActionState ActionState; // 행동에 따른 게이지 상태
+
+    }
+
+    [Serializable]
+    public class ImageSet
+    {
+        [Header("Default")]
+        [Tooltip("방향 표시기 중앙 위치값.")]
+        public Sprite[] IndicatorSprites = new Sprite[3];
+    }
+}
 public class PlayerGuardIndicator : MonoBehaviour
 {
-    [Header("IndicatorAnchor")]
-    [SerializeField] private Transform m_IndicatorAnchor;
-    [Tooltip("방향 표시기 중앙 위치값")]
-    [SerializeField] private Vector3 m_IndicatorPos;
+        [SerializeField] private GuardIndicator.Components m_components = new();
+        [SerializeField] private GuardIndicator.CheckOption m_checkOption = new();
+        [SerializeField] private GuardIndicator.CurrentState m_currentState = new();
+        [SerializeField] private GuardIndicator.ImageSet m_imageSet = new();
 
-    [Header("UIOffset")]
-    [SerializeField] private Vector3 m_TopOffset;
-    [SerializeField] private Vector3 m_LeftOffset;
-    [SerializeField] private Vector3 m_RightOffset;
+        private GuardIndicator.Components Components => m_components;
+        private GuardIndicator.CheckOption CheckOption => m_checkOption;
+        private GuardIndicator.CurrentState States => m_currentState;
+        private GuardIndicator.ImageSet Images => m_imageSet;
 
-    [Header("RectTransform")]
-    [SerializeField] private RectTransform m_TopTrans;
-    [SerializeField] private RectTransform m_LeftTrans;
-    [SerializeField] private RectTransform m_RightTrans;
 
-    [Header("OtherComponents")]
-    //[SerializeField] private PlayerController m_Player;
-    [SerializeField] private Canvas m_Canvas;
-    [SerializeField] private Camera m_Camera;
-    [SerializeField] private CanvasGroup m_CanvasGroup;
-
-    private Coroutine m_FadeCoroutine;
-
-    private void Awake()
+        private Vector3 m_rectPosition;
+        private RectTransform m_canvasRectTransform;
+        private void Awake()
     {
-        if (!m_IndicatorAnchor)
-            Debug.LogError("[Indicator] IndicatorAnchor가 할당되지 않았습니다.", this);
+        if (!Components.RectTransform)
+            Components.RectTransform = GetComponent<RectTransform>();
 
-        if (!m_TopTrans || !m_LeftTrans || !m_RightTrans)
-            Debug.LogWarning("[Indicator] 할당되지 않은 방향 UI가 있습니다.", this);
+        if (!Components.PlayerController)
+            Components.PlayerController = GetComponentInParent<PlayerController>();
 
-        if (!m_IndicatorAnchor && transform.parent)
+
+        if (!Components.IndicatorAnchor && Components.PlayerController)
         {
-            Transform uiAnchor = transform.parent.Find("UIAnchor");
-            m_IndicatorAnchor = uiAnchor != null ? uiAnchor : transform.parent;
+            Transform uiAnchor = Components.PlayerController.transform.Find("IndicatorAnchor");
+                Components.IndicatorAnchor = uiAnchor != null ? uiAnchor : transform.parent;
         }
 
-        //if (!m_Player)
-        //    m_Player = GetComponentInParent<PlayerController>();
+        if (!Components.IndicatorAnchor)
+            Debug.LogError("[Indicator] IndicatorAnchor가 할당되지 않았습니다.", this);
 
-        if (!m_Camera)
-            m_Camera = Camera.main;
 
-        if (!m_Camera)
+        if (!Components.MainCamera)
+                Components.MainCamera = Camera.main;
+
+        if (!Components.MainCamera)
             Debug.LogError("[Indicator] 카메라가 할당되지 않았습니다.", this);
 
-        if (!m_Canvas)
-            m_Canvas = GetComponent<Canvas>();
+        if (!Components.IndicatorCanvas)
+            Components.IndicatorCanvas = GetComponentInParent<Canvas>();
 
-        if (m_Canvas)
-            m_Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        if (!Components.IndicatorCanvas)
+            Debug.LogError("[Indicator] 캔버스가 할당되지 않았습니다.", this);
 
-        if (!m_CanvasGroup)
-            m_CanvasGroup = GetComponent<CanvasGroup>();
-
-        if (!m_CanvasGroup)
-            m_CanvasGroup = gameObject.AddComponent<CanvasGroup>();
+        if (Components.IndicatorCanvas)
+            m_canvasRectTransform = Components.IndicatorCanvas.GetComponent<RectTransform>();
 
         // 포지션 갱신
-        m_IndicatorPos = m_IndicatorAnchor.position;
+        m_rectPosition = Components.RectTransform.position;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // 포지션 갱신
-        m_IndicatorAnchor.position = m_IndicatorPos;
+        m_rectPosition = Components.RectTransform.position;
 
-        // 인디케이터 업데이트
-        if (m_TopTrans)
-            RefreshIndicator(m_IndicatorPos, m_TopOffset, m_TopTrans);
-        if (m_LeftTrans)
-            RefreshIndicator(m_IndicatorPos, m_LeftOffset, m_LeftTrans);
-        if (m_RightTrans)
-            RefreshIndicator(m_IndicatorPos, m_RightOffset, m_RightTrans);
+        // 인디케이터 위치 업데이트
+        RefreshIndicator(
+            Components.IndicatorAnchor.position, 
+            CheckOption.IndicatorOffset,
+            Components.RectTransform
+        );
     }
 
-    void RefreshIndicator(Vector3 worldPos, Vector3 worldOffset, RectTransform UITrans) {
-        if (!m_Camera || !UITrans)
+    private void RefreshIndicator(Vector3 worldPos, Vector3 worldOffset, RectTransform UITrans) {
+        if (!Components.MainCamera || !UITrans || m_canvasRectTransform == null)
             return;
 
-        Vector3 worldPoint = worldPos + worldOffset;
-        Vector3 screenPoint = m_Camera.WorldToScreenPoint(worldPoint);
+        Vector3 worldPoint =
+            worldPos
+            + Components.IndicatorAnchor.up * worldOffset.y
+            + Components.IndicatorAnchor.right * worldOffset.x
+            + Components.IndicatorAnchor.forward * worldOffset.z;
+        Vector3 screenPoint = Components.MainCamera.WorldToScreenPoint(worldPoint);
 
         if (screenPoint.z <= 0f)
             return;
 
-        UITrans.position = screenPoint;
+        Camera uiCamera =
+            Components.IndicatorCanvas != null &&
+            Components.IndicatorCanvas.renderMode != RenderMode.ScreenSpaceOverlay
+            ? Components.IndicatorCanvas.worldCamera
+            : null;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            m_canvasRectTransform,
+            screenPoint,
+            uiCamera,
+            out Vector2 localPoint))
+        {
+            UITrans.anchoredPosition = localPoint;
+        }
+    }
+
+    public void SetGuardZone(CursorManager.GuardZone guardZone)
+    {
+        States.IsActive = (guardZone == CheckOption.GuardZone);
+    }
+
+    public CursorManager.GuardZone GetGuardZone()
+    {
+        return CheckOption.GuardZone;
+    }
+
+    public IndicatorActionState GetActionState()
+    {
+        return States.ActionState;
+    }
+
+    public IndicatorHealthState GetHealthState()
+    {
+        return States.HealthState;
     }
 }
 
